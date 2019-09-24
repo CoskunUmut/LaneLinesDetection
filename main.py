e273d7e8723a18119e3a4882895f687561de3074
DESKTOP-QN6V7RD\umut2 <umut.deepforest@gmail.com>
2019-09-24 15:22:16 +0200

    Tested and adjusted parameters for all 6 test images

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import helpers
 images = os.listdir("test_images/")
 
 # Load Image
-img1 = images[0]
-img1 = mpimg.imread('test_images/solidWhiteRight.jpg')
+path = images[5]
+img1 = mpimg.imread("test_images/"+path)
 
 
 # 2 Grayscale Image
@@ -18,26 +18,27 @@ gray1 = helpers.grayscale(img1)
 result = gray1
 
 # 3 Canny
-canny1 = helpers.canny(result, 250, 400)
+canny1 = helpers.canny(result, 200, 350)
 result = canny1
 
+
 # 1 Region of Interest
-vertices = np.array([((50, 540), (440, 300), (540, 300), (950, 540))])
+vertices = np.array([((50, 540), (440, 325), (540, 325), (950, 540))])
 roi1 = helpers.region_of_interest(result, vertices)
 result = roi1
 
 # 4 Hough Transformation
-hough1 = helpers.hough_lines(result, 1, np.pi / 180, 50, 50, 500)
+hough1 = helpers.hough_lines(result, 1, np.pi / 180, 25, 25, 200)
+result = hough1
 
-# # 3 Gaussian Blur
-# gblur1 = helpers.gaussian_blur(result, 5)
-# result = gblur1
+result = helpers.weighted_img(result, img1, 1.0, 1.0)
 
 # Show result
-#cv2.imshow("canny", canny1)
+cv2.imshow("canny", canny1)
 # cv2.imshow("glbur", gblur1)
-cv2.imshow("roi", roi1)
+#cv2.imshow("roi", roi1)
 cv2.imshow("hough", hough1)
+cv2.imshow("Lane Line Detection", result)
 
 
 if cv2.waitKey(0) & 0xFF == ord('q'):
