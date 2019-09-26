8d60139a070474864d70e82ffde82d8ff3336b1e
CremeFresh <umut91c@gmail.com>
2019-09-26 13:12:17 +0200

    Improved lane detection with color threshold

--- main.py
+++ main.py
@@ -12,52 +12,56 @@ import helpers
 # path = images[5]
 # img1 = cv2.imread("test_images/"+path)
 #cap = cv2.VideoCapture("test_videos/solidWhiteRight.mp4")
-#cap = cv2.VideoCapture("test_videos/challenge.mp4")
-cap = cv2.VideoCapture("test_videos/solidYellowLeft.mp4")
+cap = cv2.VideoCapture("test_videos/challenge.mp4")
+#cap = cv2.VideoCapture("test_videos/solidYellowLeft.mp4")
 #cap = cv2.VideoCapture("test_videos/bs.mp4")
+
+"""Settings"""
+lower_yellow = np.array([10, 85, 150])
+up_yellow = np.array([40, 255, 255])
+sensivity = 30
+lower_white = np.array([0, 0, 255-sensivity])
+up_white = np.array([255, sensivity, 255])
+
+"""Resize"""
 _, img1 = cap.read()
 height, width, _ = img1.shape
 width = int(width/1.2)
 height = int(height/1.2)
+
+"""Region of Interest"""
+vertices = np.array(
+    [((int(width*0.05), int(height*0.90)),
+        (int(width*0.4), int(height*0.6)),
+        (int(width*0.6), int(height*0.6)),
+        (int(width*0.95), int(height*0.90)))])
+
+"""Main Loop"""
 while(cap.isOpened()):
-        # Capture frame-by-frame
+    # Capture frame-by-frame
     ret, img1 = cap.read()
     if not ret:
         break
 
     img1 = cv2.resize(img1, (width, height),
                       interpolation=cv2.INTER_AREA)
-
     result = img1
-
-    # # 1.1 Thresholding
-    _, result = cv2.threshold(img1, 200, 255, cv2.THRESH_BINARY)
-
-    # # # 2 Canny
-    result = cv2.Canny(result, 50, 200)
+    # 1 Color Thresholding
+    hsv = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
+    mask_yellow = cv2.inRange(hsv, lower_yellow, up_yellow)
+    mask_white = cv2.inRange(hsv, lower_white, up_white)
+    mask = mask_white+mask_yellow
+    # 2 Canny
+    result = cv2.Canny(mask, 50, 200)
     canny = result
-    # RoI Left
-    vertices_left = np.array(
-        [((int(width*0.1), int(height*0.9)),
-          (int(width*0.45), int(height*0.6)),
-            (int(width*0.55), int(height*0.6)),
-            (int(width*0.35), int(height*0.9)))])
-    result_left = helpers.region_of_interest(result, vertices_left)
-    # RoI Right
-    vertices_right = np.array(
-        [((int(width*0.9), int(height*0.9)),
-          (int(width*0.55), int(height*0.60)),
-            (int(width*0.45), int(height*0.60)),
-            (int(width*0.65), int(height*0.9)))])
-    result_right = helpers.region_of_interest(result, vertices_right)
-    test2r = result_right
-    test2l = result_left
+    # 3 Region of Interest
+    result = helpers.region_of_interest(result, vertices)
+    roi = result
     # 3 Hough Transform
     try:
-        result_left = helpers.hough_lines(
-            result_left, 1, np.pi/180, 15, 25, 75)  # hier noch interpolieren
-        result_right = helpers.hough_lines(
-            result_right, 1, np.pi/180, 15, 25, 75)  # hier noch interpolieren
+        result = helpers.hough_lines(
+            result, 1, np.pi/180, 15, 25, 75)  # hier noch interpolieren
+
         # # 4 GrayScale
         # result_left = helpers.grayscale(result_left)
         # result_right = helpers.grayscale(result_right)
@@ -90,28 +94,17 @@ while(cap.isOpened()):
         #     x1l = int((y1-bl) / ml)
         #     cv2.line(result, (x1r, y1), (x2r, y2), (250, 255, 255), 5)
         #     cv2.line(result, (x1l, y1), (x2l, y2), (250, 255, 255), 5)
-
-        # Print Hough Lines on Original Image
-
-        result = helpers.weighted_img(result_right, img1, 1, 1.0)
-        result = helpers.weighted_img(result_left, result, 1, 1.0)
-        cv2.imshow("Lane Line Detection", result)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     except:
-        cv2.imshow("Normal",   img1)
-        print("test")
-
         pass
 
-    # # Show result
-    # cv2.imshow("Lane Line Detection", img1)
-    # cv2.imshow("Thresh", thres1)
-    # plt.imshow(img1)
-    # plt.show()
-    #cv2.imshow('frame', result)
+    # Put Hough Lines on original image
+    result = helpers.weighted_img(result, img1, 1, 1.0)
 
+    # Show Result
+    cv2.imshow("Lane Line Detection", result)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
