adeef6bf1cc3182ac816b49919bbe3a1b3334681
DESKTOP-QN6V7RD\umut2 <umut.deepforest@gmail.com>
2019-09-27 14:33:11 +0200

    Linear Lane Curve Fitting works

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ import math
 import helpers
 import collections
 
+
 # images = os.listdir("test_images/")
 # Load Image
 # path = images[5]
 # img1 = cv2.imread("test_images/"+path)
-cap = cv2.VideoCapture("test_videos/solidWhiteRight.mp4")
-#cap = cv2.VideoCapture("test_videos/challenge.mp4")
+#cap = cv2.VideoCapture("test_videos/solidWhiteRight.mp4")
+cap = cv2.VideoCapture("test_videos/challenge.mp4")
 #cap = cv2.VideoCapture("test_videos/solidYellowLeft.mp4")
 
 
 """Settings"""
-lower_yellow = np.array([10, 85, 150])
+lower_yellow = np.array([10, 125, 175])
 up_yellow = np.array([40, 255, 255])
 sensivity = 30
 lower_white = np.array([0, 0, 255-sensivity])
@@ -45,6 +46,24 @@ vertices_right = np.array(
         (int(width*0.55), int(height*0.60)),
         (int(width*0.525), int(height*0.6)),
         (int(width*0.6), int(height*1)))])
+
+"""Lane Stabilizer"""
+bl = []
+br = []
+ml = []
+mr = []
+check_values = 40
+
+
+def get_mean(mylist, new_value):
+    if(len(mylist) == 0):
+        for i in range(check_values):
+            mylist.append(new_value)
+    else:
+        del mylist[0]
+        mylist.append(new_value)
+    return np.median(mylist)
+
 
 """Main Loop"""
 while(cap.isOpened()):
@@ -77,11 +96,14 @@ while(cap.isOpened()):
             break
     except:
         pass
-
+        # kernel = np.ones((5, 5), np.uint8)
+        # result = cv2.morphologyEx(result, cv2.MORPH_CLOSE,
+        #                           kernel, dst=None, anchor=None, iterations=20)
+    gray = helpers.grayscale(result)
     dy = 15
     dx = 15
-    counter = 0
-    gray = helpers.grayscale(result)
+    # counter = 0
+    # gray = helpers.grayscale(result)
     xl = []
     yl = []
     xr = []
@@ -89,47 +111,56 @@ while(cap.isOpened()):
     for y in range(height-1, int(height*0.6), -dy):
         for x in range(0, int(width/2), dx):
             if(gray[y][x] > 150):
-                for ddx in range(x, int(width/2), 5):
+                for ddx in range(x, int(width/2), 2):
                     if(gray[y-dy*2][ddx] > 150):
                         xl.append(ddx)
                         yl.append(y-dy*2)
         for x in range(width-1, int(width/2), -dx):
             if(gray[y][x] > 150):
-                for ddx in range(x, int(width/2), -5):
+                for ddx in range(x, int(width/2), -2):
                     if(gray[y-dy*2][ddx] > 150):
                         xr.append(ddx)
                         yr.append(y-dy*2)
     # for i in range(len(pts_left)):
     #     cv2.line(img1, pts_left[i], pts_left2[i], (255, 255, 0), 1)
+    m = 0
+    b = 0
+    """Draw left lane"""
+    m, b = np.polyfit(xl, yl, 1)
+    bl.append(b)
+    if(abs(m) > 0.2 and abs(m) < 2):
+        ml.append(m)
+    b = get_mean(bl, b)
+    m = get_mean(ml, m)
 
-    # pl = np.polyfit(xl, yl, 2)
-    # for x1 in range(int(width*0.1), int(width*0.45), dx):
-    #     x2 = x1 + dx
-    #     y2 = int(pl[0]*x2*x2+pl[1]*x2+pl[2])
-    #     y1 = int(pl[0]*x1*x1+pl[1]*x1+pl[2])
-    #     cv2.line(img1, (x2, y2), (x1, y1), (250, 255, 0), 5)
-    pr = np.polyfit(xr, yr, 2)
-    for x1 in range(int(width*0.9), int(width*0.525), -dx):
-        x2 = x1 + dx
-        y2 = int(pr[0]*x2*x2+pr[1]*x2+pr[2])
-        y1 = int(pr[0]*x1*x1+pr[1]*x1+pr[2])
+    for y1 in range(int(height), int(height*0.625), -dy):
+        y2 = y1 - dy
+        x1 = int((y1-b)/m)
+        x2 = int((y2-b)/m)
         cv2.line(img1, (x2, y2), (x1, y1), (250, 255, 0), 5)
+
+    """Draw right lane"""
+    if(not len(xr) == 0):
+        m, b = np.polyfit(xr, yr, 1)
+        br.append(b)
+        if(abs(m) > 0.2 and abs(m) < 2):
+            mr.append(m)
+        b = get_mean(br, b)
+        m = get_mean(mr, m)
+    else:
+        b = get_mean(br, br[len(br)-1])
+        m = get_mean(mr, mr[len(mr)-1])
+
+    for y1 in range(int(height), int(height*0.625), -dy):
+        y2 = y1 - dy
+        x1 = int((y1-b)/m)
+        x2 = int((y2-b)/m)
         cv2.line(img1, (x2, y2), (x1, y1), (250, 255, 0), 5)
-
-    # for x in range(width, int(width/2), -dx):
-    #     print("test2")
-    # m, b = np.polyfit(pts_left[0], pts_left[1], 1)
-    # for y in range(height-1, int(height/2), -dy):
-
-    #         if(gray[y][x] > 150):
-
-    # Put Hough Lines on original image
-    # result = helpers.weighted_img(result, img1, 1, 1.0)
 
     # Show Result
     cv2.imshow("Lane Line Detection", img1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        continue
 
 
 cap.release()
